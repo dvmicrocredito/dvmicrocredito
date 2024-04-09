18468677c7622eaacb5c7ebb3fd40d91b16407a4
dvmicrocredito <dvmicrocredito@gmail.com>
2024-04-09 05:41:44 +0200

    first commitmyy

--- pages/entradaSaida.tsx
+++ pages/entradaSaida.tsx
@@ -21,7 +21,7 @@ import {
 } from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-// kkkkkk
+// kkkkkkkkk
 const EntradasSaidas = () => {
   const { colorMode } = useColorMode();
 
