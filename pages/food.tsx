f0151f369e65c3f885b1348225ac7bd81a902cf8
dvmicrocredito <dvmicrocredito@gmail.com>
2024-04-16 16:18:43 +0200

    first commitmyytjlkkk

--- pages/food.tsx
+++ pages/food.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Heading,
@@ -20,10 +20,10 @@ import {
   VStack,
   Link,
   useColorMode,
-} from '@chakra-ui/react';
-import { SearchIcon, ExternalLinkIcon } from '@chakra-ui/icons';
-import NavBar from '@/components/NavBar';
-import Footer from '@/components/Footer';
+} from "@chakra-ui/react";
+import { SearchIcon, ExternalLinkIcon } from "@chakra-ui/icons";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 type Product = {
   code: string;
@@ -41,22 +41,24 @@ type Product = {
 };
 
 const FoodSearch: React.FC = () => {
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [searchResults, setSearchResults] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const { colorMode } = useColorMode();
-
+  // testeee
   useEffect(() => {
     if (searchTerm) {
       setLoading(true);
-      fetch(`https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&json=1`)
+      fetch(
+        `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&json=1`
+      )
         .then((response) => response.json())
         .then((data) => {
           setSearchResults(data.products);
           setLoading(false);
         })
         .catch((error) => {
-          console.error('Error fetching food data:', error);
+          console.error("Error fetching food data:", error);
           setLoading(false);
         });
     }
@@ -66,19 +68,29 @@ const FoodSearch: React.FC = () => {
     if (product.nutriments && product.nutriments[nutrientName]) {
       return product.nutriments[nutrientName];
     }
-    return 'N/A';
+    return "N/A";
   };
 
   return (
-    <Box bg={colorMode === 'light' ? 'gray.100' : 'gray.800'} minH="100vh">
+    <Box bg={colorMode === "light" ? "gray.100" : "gray.800"} minH="100vh">
       <NavBar />
       <Center>
         <Box width="100%" maxW="lg" boxShadow="lg" rounded="lg" p={6} mt={8}>
-          <Heading as="h2" size="xl" mb={4} color={colorMode === 'light' ? 'gray.700' : 'teal.500'}>
+          <Heading
+            as="h2"
+            size="xl"
+            mb={4}
+            color={colorMode === "light" ? "gray.700" : "teal.500"}
+          >
             Medicine and Nutrition Information
           </Heading>
-          <Text fontSize="lg" color={colorMode === 'light' ? 'gray.600' : 'gray.400'} mb={4}>
-            Search for detailed information about medicines, nutrition, and food products.
+          <Text
+            fontSize="lg"
+            color={colorMode === "light" ? "gray.600" : "gray.400"}
+            mb={4}
+          >
+            Search for detailed information about medicines, nutrition, and food
+            products.
           </Text>
           <Input
             placeholder="Search for food products"
@@ -106,7 +118,7 @@ const FoodSearch: React.FC = () => {
                 <Accordion key={product.code} defaultIndex={[]} allowToggle>
                   <AccordionItem>
                     <AccordionButton>
-                      <Text>{product.product_name || 'N/A'}</Text>
+                      <Text>{product.product_name || "N/A"}</Text>
                     </AccordionButton>
                     <AccordionPanel>
                       <Table variant="striped">
@@ -119,37 +131,39 @@ const FoodSearch: React.FC = () => {
                         <Tbody>
                           <Tr>
                             <Td>Barcode:</Td>
-                            <Td>{product.code || 'N/A'}</Td>
+                            <Td>{product.code || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Common name:</Td>
-                            <Td>{product.product_name || 'N/A'}</Td>
+                            <Td>{product.product_name || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Quantity:</Td>
-                            <Td>{product.quantity || 'N/A'}</Td>
+                            <Td>{product.quantity || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Packaging:</Td>
-                            <Td>{product.packaging || 'N/A'}</Td>
+                            <Td>{product.packaging || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Brands:</Td>
-                            <Td>{product.brands || 'N/A'}</Td>
+                            <Td>{product.brands || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Categories:</Td>
-                            <Td>{product.categories || 'N/A'}</Td>
+                            <Td>{product.categories || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Labels, certifications, awards:</Td>
                             <Td>
-                              {product.labels_tags ? product.labels_tags.join(', ') : 'N/A'}
+                              {product.labels_tags
+                                ? product.labels_tags.join(", ")
+                                : "N/A"}
                             </Td>
                           </Tr>
                           <Tr>
                             <Td>Manufacturing or processing places:</Td>
-                            <Td>{product.manufacturing_places || 'N/A'}</Td>
+                            <Td>{product.manufacturing_places || "N/A"}</Td>
                           </Tr>
                           <Tr>
                             <Td>Link to the product page:</Td>
@@ -160,7 +174,7 @@ const FoodSearch: React.FC = () => {
                                     Official Site <ExternalLinkIcon mx="2px" />
                                   </>
                                 ) : (
-                                  'N/A'
+                                  "N/A"
                                 )}
                               </Link>
                             </Td>
@@ -168,15 +182,17 @@ const FoodSearch: React.FC = () => {
                           <Tr>
                             <Td>Stores:</Td>
                             <Td>
-                              {Array.isArray(product.stores) ? product.stores.join(', ') : 'N/A'}
+                              {Array.isArray(product.stores)
+                                ? product.stores.join(", ")
+                                : "N/A"}
                             </Td>
                           </Tr>
                           <Tr>
                             <Td>Countries where sold:</Td>
                             <Td>
                               {Array.isArray(product.countries)
-                                ? product.countries.join(', ')
-                                : 'N/A'}
+                                ? product.countries.join(", ")
+                                : "N/A"}
                             </Td>
                           </Tr>
                           <Tr>
@@ -193,50 +209,81 @@ const FoodSearch: React.FC = () => {
                                   <Tr>
                                     <Td>Energy (kcal)</Td>
                                     <Td>
-                                      {getNutrientValue(product, 'energy-kcal') + ' kcal' || 'N/A'}
+                                      {getNutrientValue(
+                                        product,
+                                        "energy-kcal"
+                                      ) + " kcal" || "N/A"}
                                     </Td>
                                   </Tr>
                                   <Tr>
                                     <Td>Energy (kJ)</Td>
                                     <Td>
-                                      {getNutrientValue(product, 'energy-kj') + ' kJ' || 'N/A'}
+                                      {getNutrientValue(product, "energy-kj") +
+                                        " kJ" || "N/A"}
                                     </Td>
                                   </Tr>
                                   <Tr>
                                     <Td>Fat (g)</Td>
-                                    <Td>{getNutrientValue(product, 'fat') || 'N/A'}</Td>
+                                    <Td>
+                                      {getNutrientValue(product, "fat") ||
+                                        "N/A"}
+                                    </Td>
                                   </Tr>
                                   <Tr>
                                     <Td>Saturated fat (g)</Td>
-                                    <Td>{getNutrientValue(product, 'saturated-fat') || 'N/A'}</Td>
-                                  </Tr>
-                                  <Tr>
-                                    <Td>Carbohydrates (g)</Td>
-                                    <Td>{getNutrientValue(product, 'carbohydrates') || 'N/A'}</Td>
-                                  </Tr>
-                                  <Tr>
-                                    <Td>Sugars (g)</Td>
-                                    <Td>{getNutrientValue(product, 'sugars') || 'N/A'}</Td>
-                                  </Tr>
-                                  <Tr>
-                                    <Td>Fiber (g)</Td>
-                                    <Td>{getNutrientValue(product, 'fiber') || 'N/A'}</Td>
-                                  </Tr>
-                                  <Tr>
-                                    <Td>Proteins (g)</Td>
-                                    <Td>{getNutrientValue(product, 'proteins') || 'N/A'}</Td>
-                                  </Tr>
-                                  <Tr>
-                                    <Td>Salt (g)</Td>
-                                    <Td>{getNutrientValue(product, 'salt') || 'N/A'}</Td>
-                                  </Tr>
-                                  <Tr>
-                                    <Td>Fruits, vegetables, nuts, and oils (%)</Td>
                                     <Td>
                                       {getNutrientValue(
                                         product,
-                                        'fruits-vegetables-nuts-estimate-from-ingredients-list-analysis'
-                                      ) || 'N/A'}
+                                        "saturated-fat"
+                                      ) || "N/A"}
+                                    </Td>
+                                  </Tr>
+                                  <Tr>
+                                    <Td>Carbohydrates (g)</Td>
+                                    <Td>
+                                      {getNutrientValue(
+                                        product,
+                                        "carbohydrates"
+                                      ) || "N/A"}
+                                    </Td>
+                                  </Tr>
+                                  <Tr>
+                                    <Td>Sugars (g)</Td>
+                                    <Td>
+                                      {getNutrientValue(product, "sugars") ||
+                                        "N/A"}
+                                    </Td>
+                                  </Tr>
+                                  <Tr>
+                                    <Td>Fiber (g)</Td>
+                                    <Td>
+                                      {getNutrientValue(product, "fiber") ||
+                                        "N/A"}
+                                    </Td>
+                                  </Tr>
+                                  <Tr>
+                                    <Td>Proteins (g)</Td>
+                                    <Td>
+                                      {getNutrientValue(product, "proteins") ||
+                                        "N/A"}
+                                    </Td>
+                                  </Tr>
+                                  <Tr>
+                                    <Td>Salt (g)</Td>
+                                    <Td>
+                                      {getNutrientValue(product, "salt") ||
+                                        "N/A"}
+                                    </Td>
+                                  </Tr>
+                                  <Tr>
+                                    <Td>
+                                      Fruits, vegetables, nuts, and oils (%)
+                                    </Td>
+                                    <Td>
+                                      {getNutrientValue(
+                                        product,
+                                        "fruits-vegetables-nuts-estimate-from-ingredients-list-analysis"
+                                      ) || "N/A"}
                                     </Td>
                                   </Tr>
                                 </Tbody>
@@ -259,4 +306,3 @@ const FoodSearch: React.FC = () => {
 };
 
 export default FoodSearch;
-
