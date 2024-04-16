7303d22e368444c8ddb7d363d17f19f4cf442e84
dvmicrocredito <dvmicrocredito@gmail.com>
2024-04-16 19:20:45 +0200

    Updates 2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      checks: ["none"],
     }),
     // ...add more providers here
   ],
